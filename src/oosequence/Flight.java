6ca6325db732097072a95d85c51dda84c079f415
ChristopherThomson <christopher.thomson@ucalgary.ca>
2023-02-21 20:33:07 +0000

    Updated Flight for new Flight tests in part one of CC encapsulation

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -3,28 +3,8 @@ package oosequence;
 import java.util.Date;
 
 public class Flight {
-	public Date departure;
-	public Date arrival;
-    
-	/**
-     * Creates a new Flight object with the given departure and arrival dates.
-     * If either argument (or both) is null, the instance variables are set directly to the arguments.
-     * If neither argument is null, checks that the argument for the departure is before the argument for the arrival.
-     * If the departure is not before the arrival, then the departure and arrival instance variables remain null.
-     *
-     * @param departure the date of the flight's departure
-     * @param arrival the date of the flight's arrival
-     */
-    public Flight(Date departure, Date arrival) {
-        if (departure == null || arrival == null) {
-        	this.departure = departure;
-            this.arrival = arrival;
-        } else if (departure.before(arrival)) {
-        	this.departure = departure;
-            this.arrival = arrival;
-        }
-    	
-    }
+    private Date departure;
+    private Date arrival;
     
     /**
      * Creates a new Flight object with the given departure and arrival dates.
@@ -35,6 +15,16 @@ public class Flight {
      * @param departure the date of the flight's departure
      * @param arrival the date of the flight's arrival
      */
+    public Flight(Date departure, Date arrival) {
+        if (departure == null || arrival == null) {
+            this.departure = departure;
+            this.arrival = arrival;
+        } else if (departure.before(arrival)) {
+            this.departure = departure;
+            this.arrival = arrival;
+        }
+    }
+    
     /**
      * Creates a new Flight object that is a copy of the given Flight object.
      *
@@ -52,12 +42,35 @@ public class Flight {
      * @return the length of the flight in minutes, or 0 if either the departure or arrival instance variable is null.
      */
     public long length() {
-    	if (departure == null || arrival == null) {
-    		return 0;
-    	} else {
-    		long expectedLength = arrival.getTime() - departure.getTime();
-    		return expectedLength / (1000*60);
-    	}
+        if (departure == null || arrival == null) {
+            return 0;
+        } else {
+            long expectedLength = arrival.getTime() - departure.getTime();
+            return expectedLength / (1000*60);
+        }
+    }
+
+    // Getter methods
+    public Date getDeparture() {
+        return departure;
+    }
+
+    public Date getArrival() {
+        return arrival;
+    }
+
+    // Setter methods
+    public void setDeparture(Date departure) {
+        if (departure == null || (arrival == null || departure.before(arrival))) {
+            this.departure = departure;
+        }
+    }
+
+    public void setArrival(Date arrival) {
+        if (arrival == null || (departure == null || arrival.after(departure))) {
+            this.arrival = arrival;
+        }
     }
 }
+
 
---
+++ src/oosequence/Itinerary.java
@@ -0,0 +1,5 @@
+package oosequence;
+
+public class Itinerary {
+
+}
