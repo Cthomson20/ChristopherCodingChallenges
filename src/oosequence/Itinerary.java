53811829a5b606ed62563c1335dc03433c963530
ChristopherThomson <christopher.thomson@ucalgary.ca>
2023-03-10 17:28:38 -0700

    updated to the completion of Polymorphism CC

--- src/oosequence/Itinerary.java
+++ src/oosequence/Itinerary.java
@@ -11,49 +11,26 @@ public class Itinerary {
         tripComponents = new ArrayList<>();
     }
 
-    /**
-     * This code adds the flights to the list of flights in chronological order
-     * checks to see if any of the flights are overlapping
-     *
-     * @param f adds flight f to the list of flights
-     */
     public void addTripComponent(TripComponent f) {
         if (tripComponents.size() == 0) {
             tripComponents.add(f);
-            return;
-        }
-        for (int i = 0; i < tripComponents.size(); i++) {
-            TripComponent current = tripComponents.get(i);
-            if (f.getEnd().compareTo(current.getStart()) < 0) {
-                tripComponents.add(i, f);
-                removeOverlappingFlights();
-                return;
+        } else {
+            boolean added = false;
+            boolean conflict = false;
+            for (int i = 0; i < tripComponents.size() && !added && !conflict; i++) {
+                TripComponent current = tripComponents.get(i);
+                if (f.overlapsWith(current)) {
+                    conflict = true;
+                } else if (f.isBefore(current)) {
+                    tripComponents.add(i, f);
+                    added = true;    
+                }
             }
-        }
-        tripComponents.add(f);
-        removeOverlappingFlights();
-    }
-
-
-    /**
-     * It removes any overlapping flights in the list of flights
-     * the list has to be more than 1 flight to execute
-     */
-    private void removeOverlappingFlights() {
-        if (tripComponents.size() < 2) {
-            return;
-        }
-        for (int i = 1; i < tripComponents.size(); i++) {
-            TripComponent previous = tripComponents.get(i - 1);
-            TripComponent current = tripComponents.get(i);
-            if (current.getStart().compareTo(previous.getEnd()) < 0) {
-                tripComponents.remove(current);
-                i--;
+            if (!added && !conflict) {
+                tripComponents.add(f);
             }
         }
     }
-
-
     public ArrayList<TripComponent> getTripComponents() {
         return tripComponents;
     }
--- src/oosequence/TripComponent.java
+++ src/oosequence/TripComponent.java
@@ -10,32 +10,21 @@ public class TripComponent {
     private Date end;
 
     public TripComponent(Date start, Date end) {
-        if (start == null || end == null || start.after(end)) {
-            this.start = null;
-            this.end = null;
-        } else {
-            this.start = new Date(start.getTime());
-            this.end = new Date(end.getTime());
-        }
+    	setStart(start);
+    	setEnd(end);
     }
 
 
     public TripComponent(TripComponent toCopy) {
-        if (toCopy == null) {
-            this.start = null;
-            this.end = null;
-        } else {
-            this.start = toCopy.start != null ? new Date(toCopy.start.getTime()) : null;
-            this.end = toCopy.end != null ? new Date(toCopy.end.getTime()) : null;
-        }
+    	setStart(toCopy.start);
+    	setEnd(toCopy.end);
     }
 
     public String getStart() {
     	if (start == null) {
             return "";
         }
-        DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-        return dateFormat.format(start);
+        return start.toString(); 
     }
 
 
@@ -43,20 +32,20 @@ public class TripComponent {
         if (end == null) {
             return "";
         }
-        DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-        return dateFormat.format(end);
+       
+        return end.toString();
     }
 
     public void setStart(Date start) {
-        if (end == null || start == null || start.before(end)) {
-            this.start = start != null ? new Date(start.getTime()) : null;
+        if (start!=null && (end == null || start.before(end))) {
+            this.start = new Date(start.getTime());
         }
     }
 
 
     public void setEnd(Date end) {
-        if (end == null || (start == null && end.after(new Date())) || (start != null && end.after(start))) {
-            this.end = end != null ? new Date(end.getTime()) : null;
+        if (end != null && (start == null || end.after(start))) {
+            this.end = new Date(end.getTime());
         }
     }
 
@@ -71,43 +60,19 @@ public class TripComponent {
     }
 
     public boolean isBefore(TripComponent otherComponent) {
-        if (otherComponent == null || otherComponent.getStart() == null) {
-            return false;
-        }
-
-        DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-        Date otherStart;
-        try {
-            otherStart = dateFormat.parse(otherComponent.getStart());
-        } catch (ParseException e) {
-            return false;
-        }
-
-        if (end == null) {
-            return true;
-        }
-
-        return end.before(otherStart);
+    	return end.before(otherComponent.start);
+        
     }
 
     public boolean overlapsWith(TripComponent other) {
-        if (other == null || start == null || end == null || other.getStart() == null || other.getEnd() == null) {
+        if (other == null || start == null || end == null || other.start == null || other.end == null) {
             return false;
         }
-
-        DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-        Date otherStart, otherEnd;
-        try {
-            otherStart = dateFormat.parse(other.getStart());
-            otherEnd = dateFormat.parse(other.getEnd());
-        } catch (ParseException e) {
-            return false;
-        }
-
-        if (otherEnd.before(start) || otherStart.after(end)) {
-            return false;
-        }
-
-        return true;
+        if(start.before(other.start) && end.after(other.start))
+        	return true;
+        if(other.start.before(start) && other.end.after(start))
+        	return true;
+        
+        return false;
     }
 }
